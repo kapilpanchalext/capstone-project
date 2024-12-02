e3b2344f88d772cda71b24ca85bc84490141380b
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-12-02 14:40:47 +0530

    [auth server] spring boot auth server

---
+++ Dashboard-2/src/main/java/com/java/dashboard/Dashboard2Application.java
@@ -0,0 +1,13 @@
+package com.java.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dashboard2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dashboard2Application.class, args);
+	}
+
+}
---
+++ Dashboard-2/src/test/java/com/java/dashboard/Dashboard2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dashboard2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
