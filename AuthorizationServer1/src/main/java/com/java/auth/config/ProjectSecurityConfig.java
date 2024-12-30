0e9e55a8117272957695bf81586f5ee55b49cd3c
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-12-30 14:08:38 +0530

    [refactor]

--- AuthorizationServer1/src/main/java/com/java/auth/config/ProjectSecurityConfig.java
+++ AuthorizationServer1/src/main/java/com/java/auth/config/ProjectSecurityConfig.java
@@ -115,21 +115,6 @@ public class ProjectSecurityConfig {
 		return http.build();
 	}
 	
-//	@Bean
-//    @Order(2)
-//    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-//            throws Exception {
-//        http
-//                .authorizeHttpRequests((authorize) -> authorize
-//                        .anyRequest().authenticated()
-//                )
-//                // Form login handles the redirect to the login page from the
-//                // authorization server filter chain
-//                .formLogin(Customizer.withDefaults());
-//
-//        return http.build();
-//    }
-	
 	@Bean 
 	RegisteredClientRepository registeredClientRepository() {
 		RegisteredClient clientCredClient = RegisteredClient.withId(UUID.randomUUID().toString())
