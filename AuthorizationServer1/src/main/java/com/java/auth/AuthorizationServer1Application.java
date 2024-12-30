4f823de4288eb2127b47d733675fd90d1d087a54
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-12-30 08:18:00 +0530

    [initialize] authorization server

---
+++ AuthorizationServer1/src/main/java/com/java/auth/AuthorizationServer1Application.java
@@ -0,0 +1,13 @@
+package com.java.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServer1Application.class, args);
+	}
+
+}
---
+++ AuthorizationServer1/src/test/java/com/java/auth/AuthorizationServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
